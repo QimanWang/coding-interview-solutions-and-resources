e4630e01acebf72cf4f42cf991c5723989410ebb
Zeema-w <wangqiman0111@gmail.com>
2019-03-14 23:31:01 -0400

    added bt traversal

--- solutions/data-structure-primer/tree/binary_tree.py
+++ solutions/data-structure-primer/tree/binary_tree.py
@@ -96,36 +96,131 @@ red black is balaned
 A degenerate (or pathological) tree:
 A Tree where every internal node has one child
 basically a linked list
-
 '''
 
+'''
+BST methods:
+insert()
+delete()
+'''
+
+
+'''
+Array implementation:
+       A(0)    
+     /   \
+    B(1)  C(2)  
+  /   \      \
+ D(3)  E(4)   F(6) 
+OR,
+      A(1)    
+     /   \
+    B(2)  C(3)  
+  /   \      \
+ D(4)  E(5)   F(7)  
+
+For first case(0—n-1),
+if (say)father=p;
+then left_son=(2*p)+1;
+and right_son=(2*p)+2;
+
+For second case(1—n),
+if (say)father=p;
+then left_son=(2*p);
+and right_son=(2*p)+1;
+'''
+
+'''
+Use cases:
+heap: tree like data structure implemented in array to be used as a priority queue
+B-,B+ tree: used to implement indexes in databases
+syntax tree: used in compilers
+K-D tree: A space partitioning tree used to organize points in K dimensional space.
+Trie: implement dictionay for prefix lookup
+suffix tree: pattern searching in a fied tex.
+'''
+
+
 # define node the building block of trees
+
+
 class Node():
 
-    def __init__ (self, value):
+    def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
-    
 
-if __name__ == '__main__': 
-    root = Node(1)  
-    root.left = Node(2)  
-    root.left.left = Node(4)  
-    root.right = Node(3)  
-    root.right.left = Node(15)  
-    root.right.right = Node(8)  
-
-    print(root.value)
-    print(root.left.value)
-    print(root.right.value)
+# insertion
 
 
+def insert():
+    pass
+# deletion
+
+
+def delete():
+    pass
+
+####################################################################################################
 # let's find a better way to traverse the tree.
-def inOrderTraversal(root):
+
+# traversal:
+# (b) Preorder (Root, Left, Right) :  1 2 4 5 3
+# (a) Inorder (Left, Root, Right) :   4 2 5 1 3
+# (c) Postorder (Left, Right, Root) : 4 5 2 3 1
+
+
+def inOrderTraversal(node,ary):
+
+    if node is not None:
+        inOrderTraversal(node.left,ary)
+        ary.append(node.value)
+        # print(str(node.value))
+        inOrderTraversal(node.right,ary)
+
+    return ary
+
+def preOrderTraversal(node,ary):
+    if node is not None:
+        ary.append(node.value)
+        preOrderTraversal(node.left,ary)
+        preOrderTraversal(node.right,ary)
     
-    if root is N:
+    return ary
+
+def postOrderTraversal(node,ary):
+    if node is not None:
+        postOrderTraversal(node.left,ary)
+        postOrderTraversal(node.right,ary)
+        ary.append(node.value)
+
+    return ary
+
+if __name__ == '__main__':
+    root = Node(1)
+    root.left = Node(2)
+    root.left.left = Node(4)
+    root.right = Node(3)
+    root.left.right = Node(5)
+
+    print(type(root))
+    print(isinstance(root, Node))
+
+    og = []
+    og.append(root.value)
+    og.append(root.left.value)
+    og.append(root.right.value)
+    og.append(root.left.left.value)
+    og.append(root.left.right.value)
+    print("og: "+str(og))
 
 
+    pre_order_ary = preOrderTraversal(root,[])
+    print("pr: "+str(pre_order_ary))
+    in_order_ary = inOrderTraversal(root,[])
+    print("in: "+str(in_order_ary))
+    post_order_ary=postOrderTraversal(root,[])
+    print("po: "+str(post_order_ary))
 
-
+    
